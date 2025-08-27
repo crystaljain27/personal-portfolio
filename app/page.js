6e50a0449e4870f97c1d135be32a4f4f782fe471
Crystal Jain <crystaljain@Sanjays-MacBook-Air-2.local>
2025-08-28 03:32:12 +0530

    checking error

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 // @flow strict
-import AboutSection from "@/app/components/homepage/about";
-// import Collaboration from "@/app/components/homepage/collaboration";
+// import AboutSection from "@/app/components/homepage/about";
+import Collaboration from "@/app/components/homepage/collaboration";
 import ContactSection from "@/app/components/homepage/contact";
 import Education from "@/app/components/homepage/education";
 import HeroSection from "@/app/components/homepage/hero-section";
@@ -12,11 +12,11 @@ export default function Home() {
   return (
     <>
       <HeroSection />
-      <AboutSection />
+      {/* <AboutSection /> */}
       <Skills />
       <Projects />
       <AdditionalWork />
-      {/* <Collaboration /> */}
+      <Collaboration />
       <Education />
       <ContactSection />
     </>
