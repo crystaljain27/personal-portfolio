b2ec6661f3f7ed545209f3a465caa1ea2788b169
Crystal Jain <crystaljain@Sanjays-MacBook-Air-2.local>
2025-08-28 03:45:26 +0530

    checking error

--- app/layout.js
+++ app/layout.js
@@ -1,33 +1,26 @@
-import { GoogleTagManager } from "@next/third-parties/google";
 import { Inter } from "next/font/google";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import Footer from "./components/footer";
-import ScrollToTop from "./components/helper/scroll-to-top";
+import { Toaster } from "react-hot-toast";
+import "./globals.css";
 import Navbar from "./components/navbar";
-import "./css/card.scss";
-import "./css/globals.scss";
+
 const inter = Inter({ subsets: ["latin"] });
 
+// Add this metadata object
 export const metadata = {
-  title: "Portfolio of Abu Said - Software Developer",
-  description:
-    "This is the portfolio of Abu Said. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+  title: "Crystal Jain - Developer Portfolio",
+  description: "Personal portfolio of Crystal Jain, a professional software developer specializing in full-stack development, data structures, and cloud computing.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ToastContainer />
+        <Toaster />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
           <Navbar />
           {children}
-          <ScrollToTop />
         </main>
-        <Footer />
       </body>
-      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,18 +1,17 @@
 // @flow strict
+import dynamic from 'next/dynamic';
 import AboutSection from "@/app/components/homepage/about";
 import Collaboration from "@/app/components/homepage/collaboration";
 import ContactSection from "@/app/components/homepage/contact";
-// import Education from "@/app/components/homepage/education";
-import dynamic from 'next/dynamic';
-
-const Education = dynamic(() => import('@/app/components/homepage/education'), {
-  ssr: false,
-});
-
 import HeroSection from "@/app/components/homepage/hero-section";
 import Projects from "@/app/components/homepage/projects";
 import Skills from "@/app/components/homepage/skills";
 import AdditionalWork from "@/app/components/homepage/additional-work";
+
+// Dynamically import the Education component with SSR turned off
+const Education = dynamic(() => import('@/app/components/homepage/education'), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
